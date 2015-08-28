2f17808f45eb56e6b213332bcc7c40df2f1d5bd0
Ilario Vigliocco <ilario.vigliocco@etiqa.it>
2015-08-28 16:09:50 +0200

    footer update

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,9 +1,24 @@
-var nome = "Ilario Vigliocco";
-var role = "Web Developper";
+var name = "Ilario Vigliocco";
+var role = "Web Developer and Quality Assurance";
+var cell = "+(39)331.531.6993";
+var address = "Via Francese 10/E Verolengo (TO)";
+var contact = "ilariovigliocco@gmail.com";
+
 var formattedName = HTMLheaderName.replace("%data%", name);
 var formattedRole = HTMLheaderRole.replace("%data%", role);
 $("#header").append(formattedName);
 $("#header").append(formattedRole);
 
+var bio = {
+	name : "Ilario Vigliocco",
+	role : "Web Developper",
+	contactInfo : "Email: ilariovigliocco@gmail.com" 
+};
 
+var setAddress = HTMLaddress.replace("%data%", address);
+$("#footerContacts").append(setAddress);
+var setMail = HTMLemail.replace("%data%", contact);
+$("#footerContacts").prepend(setMail);
+var setCell = HTMLmobile.replace("%data%", cell);
+$("#footerContacts").append(setCell);
 
