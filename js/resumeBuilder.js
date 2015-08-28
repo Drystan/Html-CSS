95746b19e10deb4175d79554f9d14cc9e1620b24
Ilario Vigliocco <ilario.vigliocco@etiqa.it>
2015-08-28 12:48:02 +0200

    changing role and name

---
+++ js/resumeBuilder.js
@@ -0,0 +1,1 @@
+$("#main").append('Ilario Vigliocco');
--- resumeBuilder.js
+++ resumeBuilder.js
@@ -1,1 +1,9 @@
-$("#main").append('Ilario Vigliocco');
+var name = "Ilario Vigliocco"
+var role = "Web Developper"
+var farmattedName = HTMLheaderName.replace(%data%, name);
+var  = HTMLheaderRole.replace(%data% role);
+$("#header").append(fotmattedName);
+$("#header").append(fotmattedRole);
+
+
+
